5a145a60b55bd16f5b315c2511c4b0e5e578fa27
Jai Vardhan <pdmnvardhan510@gmail.com>
2025-08-20 09:08:18 -0500

    empty branch

---
+++ src/pages/LocationsPage.jsx
@@ -0,0 +1,10 @@
+// import Map from "../components/map";
+
+// export default function LocationsPage() {
+//   return (
+//     <div>
+//       <h1>Nearby Gyms</h1>
+//       <Map />
+//     </div>
+//   );
+// }
